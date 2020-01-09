f1558cad3aabed15c168adb95f42e238fec75862
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-01-09 14:21:10 +0900

    added links to app, made about us and contact pages, got burger menu operational and added links between pages, improved styling

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
-console.log(1 + 2 == 3)
-console.log(1 + 2 == 3);
+(function() {
+
+    'use strict';
+  
+    document.querySelector('.material-design-hamburger__icon').addEventListener(
+      'click',
+      function() {      
+        var child;
+        
+        document.body.classList.toggle('background--blur');
+        this.parentNode.nextElementSibling.classList.toggle('menu--on');
+  
+        child = this.childNodes[1].classList;
+  
+        if (child.contains('material-design-hamburger__icon--to-arrow')) {
+          child.remove('material-design-hamburger__icon--to-arrow');
+          child.add('material-design-hamburger__icon--from-arrow');
+        } else {
+          child.remove('material-design-hamburger__icon--from-arrow');
+          child.add('material-design-hamburger__icon--to-arrow');
+        }
+  
+      });
+  
+  })();
+
+  $( '.js-input' ).keyup(function() {
+    if( $(this).val() ) {
+       $(this).addClass('not-empty');
+    } else {
+       $(this).removeClass('not-empty');
+    }
+  });
